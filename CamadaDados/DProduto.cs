7d3103f67c655d64759dfc8df2f167f5e68b54a1
Lucas Corte <corte1994.lc@gmail.com>
2021-05-24 20:34:42 -0300

    Implementação dos atributos e encapsulamento da classe

---
+++ CamadaDados/DProduto.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+//permite o uso do DataGridView
+using System.Data;
+// importa recursos do Mysql
+using MySql.Data.MySqlClient;
+using System.Data.SqlClient;
+
+
+namespace CamadaDados
+{
+    class DProduto
+    {
+        public int idproduto { get; set; }
+        public string Nomeproduto { get; set; }
+        public double Precounitario { get; set; }
+        public int QtdeEstoque { get; set; }
+        //constructor vazio
+        public DProduto() { }
+
+    }
+}
--- CamadaNegocio/NProduto.cs
+++ CamadaNegocio/NProduto.cs
@@ -3,6 +3,8 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+//Repassa dados para a camada de dados
+using CamadaDados;
 
 namespace CamadaNegocio
 {
